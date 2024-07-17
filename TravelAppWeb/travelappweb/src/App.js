59bc34c859c76455b9d90d7e18cb0e7cbc2e08b1
TimaDawaa <tima302t@gmail.com>
2024-07-17 23:13:39 +0300

    fix

--- TravelAppWeb/travelappweb/src/App.js
+++ TravelAppWeb/travelappweb/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import DashboardLayout from "./DashboardLayout/DashboardLayout.js";
-import Admins from "./Pages/Admins/Admins.js";
+import DashboardLayout from "./helper/Components/DashboardLayout/DashboardLayout.js";
 import Login from "./Pages/Login/Login.js";
 import Organizers from "./Pages/Organizers/organizers.js";
 import OrganizersDetails from "./Pages/Organizers/OrganizersList/organizersDetails.js";
@@ -10,15 +8,10 @@ import OrganizersFullDetails from "./Pages/Organizers/OrganizersList/organizersF
 import OrganizerDetailsRequest from "./Pages/Organizers/RequestsOrganizers/organizer_details_request.js";
 import AllRequest from "./Pages/Organizers/RequestsOrganizers/all_request.js";
 import { OrganizersProvider } from "./Context/organizers_context.js";
-=======
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import Admins from "./Pages/Admins/Admins.js";
-import Login from "./Pages/Login/Login.js";
 import Dashboard from "./Pages/Dashboard/Dashboard.js";
 import AddAdmin from "./Pages/Admins/AddAdmin/AddAdmin.js";
-import DashboardLayout from "./helper/Components/DashboardLayout/DashboardLayout.js";
 
->>>>>>> 9119550cfb92a5520ea7ab387314608dd2364b5f
 function App() {
   return (
     <OrganizersProvider>
@@ -28,7 +21,6 @@ function App() {
           <Route path="/" element={<DashboardLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="admins" element={<Admins />} />
-<<<<<<< HEAD
             <Route path="/Organizers" element={<Organizers />} />
             <Route
               path="/OrganizersDetails"
@@ -45,9 +37,7 @@ function App() {
               path="/OrganizerDetailsRequest"
               element={<OrganizerDetailsRequest />}
             />
-=======
             <Route path="addAdmin" element={<AddAdmin />} />
->>>>>>> 9119550cfb92a5520ea7ab387314608dd2364b5f
           </Route>
           <Route path="/login" element={<Login />} />
         </Routes>

10f99fc3ee449dcfe9a761d764c38128fa82cc98
Khalid Sinan <iidrgames1@gmail.com>
2024-07-28 16:29:46 +0300

    dashboard statistics

--- TravelAppWeb/travelappweb/src/Pages/Dashboard/Dashboard.js
+++ TravelAppWeb/travelappweb/src/Pages/Dashboard/Dashboard.js
@@ -1,25 +1,113 @@
 import React, { useContext } from "react";
-import { BarChart, pieArcLabelClasses, PieChart } from "@mui/x-charts";
+import { BarChart, LineChart, PieChart } from "@mui/x-charts";
 import DashboardContext from "../../Context/dashboard_context";
 import styles from "./Dashboard.module.css";
 import TopCountries from "./top_10_countries";
-
-const pieData = [
-  { label: "Organized", value: 40, color: "var(--primary-color)" },
-  { label: "Not Organized", value: 13, color: "grey" },
-];
+import HotelsList from "./HotelsStats/HotelsStats";
+import { CircularProgress } from "@mui/material";
+import CustomIconButton from "../../helper/Components/IconButton/CustomIconButton";
+import { BsArrowRightShort } from "react-icons/bs";
 
 const Dashboard = () => {
-  const { topCountries } = useContext(DashboardContext);
+  const { topCountries, organizedTripsPer, topHotels } =
+    useContext(DashboardContext);
   return (
     <main className={styles["statistics-section"]}>
-      <header className={styles["general-statistics"]}>
-      </header>
+      <section className={styles["general-statistics"]}>
+        <div
+          className={`${styles["statistics-card"]} ${styles["profit-stats"]}`}
+        >
+          <h2>JourneyJoy's Profits:</h2>
+          <LineChart
+            colors={["var(--primary-color)"]}
+            xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+            series={[
+              {
+                data: [2, 5.5, 2, 8.5, 1.5, 5],
+              },
+            ]}
+          />
+        </div>
+        <div
+          className={`${styles["statistics-card"]} ${styles["organized-percentage-stats"]}`}
+        >
+          <h2>Organized Trips Percentage:</h2>
+          {organizedTripsPer ? (
+            <PieChart
+              series={[
+                {
+                  data: [
+                    {
+                      label: "Organized",
+                      value: organizedTripsPer.organizedTripsCount,
+                    },
+                    {
+                      label: "Not Organized",
+                      value: organizedTripsPer.tripsCount,
+                    },
+                  ],
+                  innerRadius: 60,
+                  paddingAngle: 4,
+                  cornerRadius: 5,
+                },
+              ]}
+              margin={{ bottom: 0, left: 0, top: 0, right: 0 }}
+              colors={["var(--primary-color)", "var(--date-color)"]}
+              slotProps={{
+                legend: { hidden: true },
+              }}
+              height={180}
+            />
+          ) : (
+            <CircularProgress />
+          )}
+        </div>
+      </section>
       <section className={styles["specific-statistics"]}>
         <section className={styles["hotels-statistics"]}>
+          <div
+            className={`${styles["statistics-card"]} ${styles["hotels-list"]}`}
+          >
+            <div className={styles["hotels-list-header"]}>
+              <h2>JourneyJoy's Hotels:</h2>
+              <CustomIconButton
+                icon={
+                  <BsArrowRightShort color="var(--primary-color)" size={32} />
+                }
+              />
+            </div>
+            {topHotels ? (
+              <HotelsList hotels={topHotels} />
+            ) : (
+              <CircularProgress />
+            )}
+          </div>
+          <div
+            className={`${styles["statistics-card"]} ${styles["hotels-stats"]}`}
+          >
+            <h2>JourneyJoy's Top 10 Hotels:</h2>
+            {topHotels ? (
+              <BarChart
+                colors={["var(--primary-color)"]}
+                yAxis={[
+                  {
+                    scaleType: "band",
+                    data: topHotels.map((hotel) => hotel.name),
+                  },
+                ]}
+                margin={{ top: 20 }}
+                layout="horizontal"
+                grid={{ vertical: true }}
+                series={[
+                  { data: topHotels.map((hotel) => hotel.reservationCount) },
+                ]}
+              />
+            ) : (
+              <CircularProgress />
+            )}
+          </div>
         </section>
-        <section className={styles["countries-statistics"]}>
-        </section>
+        <section className={styles["countries-statistics"]}></section>
       </section>
     </main>
   );
@@ -85,3 +173,40 @@ export default Dashboard;
         <p>Loading...</p>
       )} */
 }
+
+// <PieChart
+//           series={[
+//             {
+//               data: pieData,
+//               faded: {
+//                 innerRadius: 30,
+//                 additionalRadius: -30,
+//               },
+//             },
+//           ]}
+//           slotProps={{
+//             legend: { hidden: true },
+//           }}
+//         />
+//         <PieChart
+//           series={[
+//             {
+//               data: pieData,
+//               faded: {
+//                 innerRadius: 30,
+//                 additionalRadius: -30,
+//               },
+//             },
+//           ]}
+//           slotProps={{
+//             legend: { hidden: true },
+//           }}
+//         />
+//         <LineChart
+//           xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+//           series={[
+//             {
+//               data: [2, 5.5, 2, 8.5, 1.5, 5],
+//             },
+//           ]}
+//         />
